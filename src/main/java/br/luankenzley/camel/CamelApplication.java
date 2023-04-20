5bbe3679d63f84ef3e80dfe209d00ca13f8e3277
Luan Kenzley <kenzleydev@outlook.com>
2023-04-20 08:38:15 -0300

    iniciando configurações iniciais

---
+++ src/main/java/br/luankenzley/camel/CamelApplication.java
@@ -0,0 +1,13 @@
+package br.luankenzley.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+
+}
